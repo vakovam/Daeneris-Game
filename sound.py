19a0410bbec10219bedc4e38f98f36a43df6d1a7
vakovam <michaela7472@gmail.com>
2016-11-25 00:04:41 +0000

    Update sound.py

--- sound.py
+++ sound.py
@@ -1,31 +1,6 @@
 #code for the penalty
-import wave
-import random
-import struct
-noise_output = wave.open('sad.wav', 'w')
-noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
-
-for i in range(0, 13230000):
-        value = random.randint(-32767, 32767)
-        packed_value = struct.pack('h', value)
-        noise_output.writeframes(packed_value)
-        noise_output.writeframes(packed_value)
-
-noise_output.close()
-
-
-
-#code for the reward
-import wave
-import random
-import struct
-noise_output = wave.open('happy.wav', 'w')
-noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
-
-for i in range(0, 13230000):
-        value = random.randint(-32767, 32767)
-        packed_value = struct.pack('h', value)
-        noise_output.writeframes(packed_value)
-        noise_output.writeframes(packed_value)
-
-noise_output.close()
+import winsound
+winsound.PlaySound('sad.wav', winsound.SND_FILENAME)
+#code for reward
+import winsound
+winsound.PlaySound('happy.wav', winsound.SND_FILENAME)
